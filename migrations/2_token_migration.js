3b2eded06852d404e9791c3afe286102abe9907f
Carson Locke Ross <carson.yaumy@gmail.com>
2022-03-06 00:52:53 +0800

    chore: remove unused parameter

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -2,7 +2,7 @@ const TokenA = artifacts.require("TokenA");
 const TokenB = artifacts.require("TokenB");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(TokenA, "Token A", "TKA");
+    await deployer.deploy(TokenA);
     const instanceA = await TokenA.deployed();
     await deployer.deploy(TokenB, instanceA.address);
 };
