d19ded916c55a874b9e4257ae79afbdab2ef2888
Carson Locke Ross <carson.yaumy@gmail.com>
2022-03-06 19:51:03 +0800

    add testnet reminder

--- app/nextjs/blockchain/wraptoken_abi.js
+++ app/nextjs/blockchain/wraptoken_abi.js
@@ -2,9 +2,9 @@ import Web3 from "web3";
 import TokenAContract from "../../../build/contracts/Rinkeby_TokenA.json";
 import TokenBContract from "../../../build/contracts/Rinkeby_TokenB.json";
 
-const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/33ff9656df9c4eb39e4bf0f114cec86b');
+// const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/33ff9656df9c4eb39e4bf0f114cec86b');
 
-const web3 = new Web3(provider)
+// const web3 = new Web3(provider)
 
 export const TokenAContractData = TokenAContract;
 export const TokenBContractData = TokenBContract;
--- app/nextjs/pages/index.js
+++ app/nextjs/pages/index.js
@@ -177,9 +177,10 @@ const Main = () => {
                     <div className="container mx-auto px-4">
                         <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
                             <div className="px-6">
+                                <h5 className='text-center mt-6 font-bold'>Try playaround with Rinkeby Testnet <a className='text-blue-500' target={"_blank"} href={'https://faucet.rinkeby.io/'}>(Faucet)</a></h5>
                                 <div className="flex flex-wrap justify-center">
                                     <div className="w-full lg:w-8/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                                        <div className="py-6 px-3 mt-6 sm:mt-0 text-center">
+                                        <div className="py-6 px-3 mt-4 sm:mt-0 text-center">
                                             <button
                                                 className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none"
                                                 type="button"
