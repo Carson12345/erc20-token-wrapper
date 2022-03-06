2cee694dab19dc8f0dfcb956c10b0fa6a5926d14
Carson Locke Ross <carson.yaumy@gmail.com>
2022-03-06 14:54:10 +0800

    connect rinkeby for test app

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const Web3 = require("web3");
-const TokenAContract = require("../build/contracts/TokenA.json");
-const TokenBContract = require("../build/contracts/TokenB.json");
+const TokenAContract = require("../build/contracts/Rinkeby_TokenA.json");
+const TokenBContract = require("../build/contracts/Rinkeby_TokenB.json");
 
 const secrets = require("../secrets.json")
 const address = '0xc6169D5224558B97fD7cec848596b6cCfD84C596';
@@ -10,7 +10,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const init = async () => {
     // const web3 = new Web3('http://localhost:7545');
 
-    const provider = new HDWalletProvider(privateKey, `https://ropsten.infura.io/v3/${secrets.projectId}`);
+    const provider = new HDWalletProvider(privateKey, `https://rinkeby.infura.io/v3/${secrets.projectId}`);
     const web3 = new Web3(provider);
 
     const id = await web3.eth.net.getId();
